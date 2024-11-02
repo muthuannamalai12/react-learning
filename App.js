b2c91850deb104044e428f85a29cd545c83f158f
Muthu Annamalai Venkatachalam <muthuannamalai2002@gmail.com>
2024-11-02 08:43:18 +0530

    Episode-2 Code Example

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // Creates a h1 tag and prints Hello World From React
 // const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello World From React!!");
 
@@ -27,17 +30,6 @@ const parent = React.createElement(
   </div>
 </div>
 */
-
-<div id="parent">
-  <div id="child1">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h2 tag</h2>
-  </div>
-  <div id="child2">
-    <h1>I'm h1 tag</h1>
-    <h2>I'm h2 tag</h2>
-  </div>
-</div>;
 
 /*Structure
 <div id="parent">
